91915b0a84f718975105c1a67baf1a96791955e6
Maxim <mkarabashev@gmail.com>
2017-01-19 23:34:42 +0200

    mongolab fix

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DB = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/test';
+exports.DB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/test';
 exports.PORT = process.env.PORT || 5000;
 exports.URL = process.env.URL || `localhost:${exports.PORT}`;
