e347ee9cb1c27bdef391eb77b8867aebc5f03014
Maxim <mkarabashev@gmail.com>
2017-01-26 21:58:57 +0200

    got rid of a console log

--- app/controllers/apiController.js
+++ app/controllers/apiController.js
@@ -20,6 +20,7 @@ const api = (req, res) => {
       if (!req.query.hasOwnProperty(fn) || !asyncActions[fn]) continue;
       actionList[fn] = asyncActions[fn].bind(null, data);
     }
+
     return actionList;
   };
 
