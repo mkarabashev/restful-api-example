28552b331ac21fe6defb7f162f80958a2a887688
Maxim <mkarabashev@gmail.com>
2017-02-06 08:35:54 +0200

    `new relic configuration

--- newrelic.js
+++ newrelic.js
@@ -14,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: process.env.NEW_RELIC || 'fail',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY || 'fail',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
